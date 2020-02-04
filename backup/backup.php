df33766108568b086b7528a2df9a147098d24fb6
Malventi, Giulio <Giulio.Malventi@nov.com>
2020-02-04 11:26:12 +0100

    Added backup

---
+++ backup/backup.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+    require_once 'workflowy-php/src/autoload.php';
+    error_reporting(E_ALL & ~E_NOTICE);
+
+    use WorkFlowyPHP\WorkFlowy;
+    use WorkFlowyPHP\WorkFlowyList;
+    use WorkFlowyPHP\WorkFlowyException;
+
+    $date = date('Ymd');
+
+    // login - ADD PASSWORD
+    $session_id = WorkFlowy::login('giulio.malventi@gmail.com', '');
+
+    // get list
+    $list_request = new WorkFlowyList($session_id);
+    $list = $list_request->getList();
+
+    // output list for backup
+    $dir = 'data/';
+    if (!is_dir($dir)) mkdir($dir, 0700, TRUE);
+    file_put_contents($dir . $date . '.opml.gz', gzencode(
+        $list->getOPML()
+    ));
+?>
