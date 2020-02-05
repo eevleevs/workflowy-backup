195259f60c88b7d7e883fb6872047f45561ebb3c
Giulio Malventi <giulio@vps782851.ovh.net>
2020-02-05 13:57:41 +0100

    Split credentials file

--- backup/backup.php
+++ backup/backup.php
@@ -1,6 +1,7 @@
 #!/usr/bin/env php
 <?php
     require_once 'workflowy-php/src/autoload.php';
+    require 'credentials.php';
     error_reporting(E_ALL & ~E_NOTICE);
 
     use WorkFlowyPHP\WorkFlowy;
@@ -10,14 +11,14 @@
     $date = date('Ymd');
 
     // login - ADD PASSWORD
-    $session_id = WorkFlowy::login('giulio.malventi@gmail.com', '');
+    $session_id = WorkFlowy::login($username, $password);
 
     // get list
     $list_request = new WorkFlowyList($session_id);
     $list = $list_request->getList();
 
     // output list for backup
-    $dir = '/data/';
+    $dir = '/app/data/';
     if (!is_dir($dir)) mkdir($dir, 0700, TRUE);
     file_put_contents($dir . $date . '.opml.gz', gzencode(
         $list->getOPML()
